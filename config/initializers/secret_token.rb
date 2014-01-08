31b257a51511d90af2931d97f5c0a216f4cd2012
Luke <luketliu@gmail.com>
2014-01-08 15:03:53 -0600

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bayes::Application.config.secret_key_base = '4e08c9ab8857247bd36e5e53e0b93a2b19070debd9ef0e3768021c97db7a1d4d478ef0ae72c72a0a793e8609c196a6b54859baed7ba0f5265a3ecb1cf3fad355'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bayes::Application.config.session_store :cookie_store, key: '_bayes_session'
